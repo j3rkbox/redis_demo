502ab54202760b9f9cd9410eb2d68eeccd85a252
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2020-11-23 19:20:47 +0000

    specs

--- core/src/main/java/io/github/coinj/Chain.java
+++ core/src/main/java/io/github/coinj/Chain.java
@@ -8,4 +8,4 @@ public interface Chain {
     KeyPair generateKeyPair();
     KeyPair generateKeyPair(String secret);
 
-    PackedRawTransaction packTransaction(RawTransaction
+    PackedRawTransaction packTransaction(RawTransaction rawTransaction) th
