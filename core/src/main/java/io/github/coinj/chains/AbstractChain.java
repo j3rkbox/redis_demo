95937e33a6cdf5ea5a40b728d36616a2497028ad
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-01-17 07:04:12 +0000

    Updated CI scripts.

--- core/src/main/java/io/github/coinj/chains/AbstractChain.java
+++ core/src/main/java/io/github/coinj/chains/AbstractChain.java
@@ -8,3 +8,11 @@ import okhttp3.OkHttpClient;
 import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractChain implements Chain {
+    protected static final OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(300, TimeUnit.SECONDS)
+            .writeTimeout(300, TimeUnit.SECONDS)
+            .readTimeout(300, TimeUnit.SECONDS)
+            .build();
+    protected static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+}
