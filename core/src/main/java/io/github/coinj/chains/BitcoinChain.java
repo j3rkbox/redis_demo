a59d9ec7a4353bfb05319e67e8effb23bc25c582
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-01-24 09:41:17 +0000

    fix makefile

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -3,4 +3,5 @@ package io.github.coinj.chains;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
-import com.google.gson.reflect.Typ
+import com.google.gson.reflect.TypeToken;
+import io.github.coinj.*;
