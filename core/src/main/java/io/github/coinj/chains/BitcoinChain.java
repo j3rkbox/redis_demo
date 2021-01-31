ff9e212569f09e5e16dbe094b687c7f4a5326db3
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-01-31 21:12:50 +0000

    update for next development version

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -15,4 +15,5 @@ import org.bitcoinj.crypto.TransactionSignature;
 import org.bitcoinj.params.MainNetParams;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.script.Script;
-import org.bitcoinj.script.Scr
+import org.bitcoinj.script.ScriptBuilder;
+import org.bi
