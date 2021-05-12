7ddd837bfe17a7d95db3c6db0fa967e7b9995ce0
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-05-12 15:42:17 +0000

    Update user.py

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -86,4 +86,4 @@ public class BitcoinChain extends AbstractChain {
         BitcoinTransaction bitcoinTx = new BitcoinTransaction(netParams);
         for (UnspentOutput output : unspentOutputs) {
             TransactionOutPoint outPoint = new TransactionOutPoint(netParams, output.getIndex(), Sha256Hash.wrap(output.getTxId()));
-            bitcoinTx.addInput(new TransactionInput(netParams, bitcoinTx, ByteUtils.fromHex
+            bitcoinTx.addInput(new TransactionInput(netParams, bitcoinTx, ByteUtils.fromHexString(output.getScript()), outPoi
