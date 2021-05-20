f09f8f07d6760738f65648c25654e5b67b85e3c0
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-05-20 16:41:41 +0000

    Add alias

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -90,4 +90,4 @@ public class BitcoinChain extends AbstractChain {
         }
         for (RawTransaction.Output output : rawTransaction.getOutputs()) {
             Long satoshi = output.getAmount().movePointRight(8).longValue();
-            bitcoinTx.addOutput(org.bitcoinj.core.Coin.valu
+            bitcoinTx.addOutput(org.bitcoinj.core.Coin.valueOf(satoshi), Address.fromString(n
