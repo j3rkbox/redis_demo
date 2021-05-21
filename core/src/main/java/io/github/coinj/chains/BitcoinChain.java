7c9075f9c856a52280b43b2e325a3819b73d618d
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-05-21 07:34:49 +0000

    release: prepare next release

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -90,4 +90,4 @@ public class BitcoinChain extends AbstractChain {
         }
         for (RawTransaction.Output output : rawTransaction.getOutputs()) {
             Long satoshi = output.getAmount().movePointRight(8).longValue();
-            bitcoinTx.addOutput(org.bitcoinj.core.Coin.valueOf(satoshi), Address.fromString(n
+            bitcoinTx.addOutput(org.bitcoinj.core.Coin.valueOf(satoshi), Address.fromString(netParams, output.getAddress())
