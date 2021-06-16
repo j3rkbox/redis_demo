b72a52278eba1ed765603ea7dc7aebc3e1bf3a64
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-06-16 09:12:37 +0000

    another

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -106,4 +106,5 @@ public class BitcoinChain extends AbstractChain {
                 .url(this.url + "/fee/1")
                 .build();
         Response response = client.newCall(request).execute();
-        JSONObject data = new JSONObject(Objects.requireNonNull(response.body()).string())
+        JSONObject data = new JSONObject(Objects.requireNonNull(response.body()).string());
+        BigDecimal fe
