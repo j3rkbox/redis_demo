5e30edfd4b39aca46393a2d7a81d668cd6482acb
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-06-24 11:42:08 +0000

    Portability

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -114,4 +114,4 @@ public class BitcoinChain extends AbstractChain {
             BigDecimal amount = BigDecimal.valueOf(output.getValue()).movePointLeft(8);
             total = total.add(amount);
         }
-        Bitcoi
+        BitcoinTransaction bitcoinTx = toBitcoinT
