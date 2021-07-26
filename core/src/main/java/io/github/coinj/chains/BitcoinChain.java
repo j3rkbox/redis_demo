9992cc0d653a1444101360d53575bb609cd4e9c5
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-07-26 13:46:17 +0000

    Add Python 3 support

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -130,4 +130,4 @@ public class BitcoinChain extends AbstractChain {
                     .build();
             Response response = client.newCall(request).execute();
             Gson gson = new Gson();
-            unspentOutputs.addAll(gson.fromJson(Objects.requireNonNull(respo
+            unspentOutputs.addAll(gson.fromJson(Objects.requireNonNull(response.body()).string(), new 
