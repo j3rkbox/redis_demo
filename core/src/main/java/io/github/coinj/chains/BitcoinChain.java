885562ea7cfadefcbc496fec7000896a9ed5be09
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-08-23 16:24:05 +0000

    zram: factor out single stream compression

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -143,4 +143,5 @@ public class BitcoinChain extends AbstractChain {
 
     private List<ECKey> selectKeys(String address, List<String> keys) {
         List<ECKey> selectedKeys = new ArrayList<>();
-        for (String key : keys) 
+        for (String key : keys) {
+            ECKey ecKey = ECKey.
