92af3ceb4243fd392432166e94056ed12241ac86
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-09-16 15:02:41 +0000

    improved docstrings

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -158,4 +158,4 @@ public class BitcoinChain extends AbstractChain {
 
     @Override
     public SignedRawTransaction signTransaction(PackedRawTransaction transaction, List<String> keys) {
-        List<UnspentOutput> unspentOutputs = (List
+        List<UnspentOutput> unspentOutputs = (List<UnspentOutput>) transactio
