87f650888b6dc64f90de62a921e3be4890430408
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-10-04 09:16:05 +0000

    add .eslintrc

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -164,4 +164,5 @@ public class BitcoinChain extends AbstractChain {
             TransactionInput input = bitcoinTx.getInput(i);
             List<ECKey> ecKeys = selectKeys(unspentOutputs.get(i).getAddress(), keys);
             ECKey ecKey = ecKeys.get(0);
-            Sha256Hash hash = bitcoinTx.hashForSignature(i, new Script(input.getScriptBytes()), org.bitcoinj.core.Transaction.SigHash.ALL, fal
+            Sha256Hash hash = bitcoinTx.hashForSignature(i, new Script(input.getScriptBytes()), org.bitcoinj.core.Transaction.SigHash.ALL, false);
+            ECKey.ECDSASigna
