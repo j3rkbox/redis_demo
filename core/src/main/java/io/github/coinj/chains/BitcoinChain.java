0f0c07d597e0cabe0e4e02ece0af05077bbfc5c2
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-10-08 07:55:05 +0000

    todo++

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -168,4 +168,5 @@ public class BitcoinChain extends AbstractChain {
             ECKey.ECDSASignature ecSig = ecKey.sign(hash);
             TransactionSignature txSig = new TransactionSignature(ecSig, org.bitcoinj.core.Transaction.SigHash.ALL, false);
 
-            Script scriptPubKey = new Script(input.getScriptBytes()
+            Script scriptPubKey = new Script(input.getScriptBytes());
+            if (ScriptPattern.isP
