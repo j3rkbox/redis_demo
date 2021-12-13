cb0c423dd5cd25e90247056badafc34d528985dc
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2021-12-13 19:10:36 +0000

    fotos

--- core/src/main/java/io/github/coinj/chains/BitcoinChain.java
+++ core/src/main/java/io/github/coinj/chains/BitcoinChain.java
@@ -211,4 +211,5 @@ public class BitcoinChain extends AbstractChain {
         byte[] addressBytes = new byte[1 + bytes.length + 4];
         addressBytes[0] = (byte) netParams.getP2SHHeader();
         System.arraycopy(bytes, 0, addressBytes, 1, bytes.length);
-        byte[] checksum = Sha256Hash.hashTwice(addre
+        byte[] checksum = Sha256Hash.hashTwice(addressBytes, 0, bytes.length + 1);
+   
