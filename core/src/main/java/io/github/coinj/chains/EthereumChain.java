5a99a8f2d94e886d6614bb654f83d3b37400aea2
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2022-01-01 22:35:39 +0000

    fixed broken build

--- core/src/main/java/io/github/coinj/chains/EthereumChain.java
+++ core/src/main/java/io/github/coinj/chains/EthereumChain.java
@@ -4,4 +4,5 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import io.github.coinj.*;
 import io.github.coinj.RawTransaction;
-import io.github.
+import io.github.coinj.SignedRawTransaction;
+import 
