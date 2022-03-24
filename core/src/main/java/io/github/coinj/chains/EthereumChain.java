2d516d4a7ae4fba767dd8ca2c046a40505b5b67b
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2022-03-24 08:34:13 +0000

    for v 0.0.1

--- core/src/main/java/io/github/coinj/chains/EthereumChain.java
+++ core/src/main/java/io/github/coinj/chains/EthereumChain.java
@@ -66,4 +66,5 @@ public class EthereumChain implements Chain {
     @Override
     public PackedRawTransaction packTransaction(RawTransaction rawTransaction) throws ExecutionException, InterruptedException {
         PackedRawTransaction packedTx = new PackedRawTransaction(rawTransaction);
-        Web3j web3 = Web3j.build(
+        Web3j web3 = Web3j.build(new HttpService(url));
+        B
