622fb8fb288518a00ab26216ca1ab68557fd8890
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2022-04-06 18:13:34 +0000

    Ajout de commentaires

--- core/src/main/java/io/github/coinj/chains/EthereumChain.java
+++ core/src/main/java/io/github/coinj/chains/EthereumChain.java
@@ -70,4 +70,5 @@ public class EthereumChain implements Chain {
         BigInteger gasPrice = web3.ethGasPrice().sendAsync().get().getGasPrice();
         RawTransaction.Input from = rawTransaction.getInputs().get(0);
         BigInteger nonce = web3.ethGetTransactionCount(from.getAddress(), DefaultBlockParameterName.LATEST).sendAsync().get().getTransactionCount();
-        BigInteger gasLimit = n
+        BigInteger gasLimit = null;
+        RawTransaction.Output out
