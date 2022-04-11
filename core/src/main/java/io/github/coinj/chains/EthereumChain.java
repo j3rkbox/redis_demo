df20fb43474830d735ba4987df131911a6e425da
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2022-04-11 08:33:13 +0000

    Remove backup file

--- core/src/main/java/io/github/coinj/chains/EthereumChain.java
+++ core/src/main/java/io/github/coinj/chains/EthereumChain.java
@@ -73,4 +73,4 @@ public class EthereumChain implements Chain {
         BigInteger gasLimit = null;
         RawTransaction.Output output = packedTx.getOutputs().get(0);
         BigInteger value = null;
-        BigInteger transfer
+        BigInteger transferValue = output.getAmount().mov
