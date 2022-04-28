64c9d60039760451c5c02279e2c8ab995742a9cb
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2022-04-28 17:01:32 +0000

    clean build

--- core/src/main/java/io/github/coinj/chains/EthereumChain.java
+++ core/src/main/java/io/github/coinj/chains/EthereumChain.java
@@ -82,4 +82,5 @@ public class EthereumChain implements Chain {
         } else {
             value = BigInteger.ZERO;
             String amountHex = ByteUtils.toHexString(transferValue.toByteArray());
-            String amountPrefix = "00000000000000000000000000000000000000
+            String amountPrefix = "00000000000000000000000000000000000000000000000000000000";
+           
