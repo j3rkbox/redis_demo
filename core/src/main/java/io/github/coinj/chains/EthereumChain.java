745886e713455188ed7fa2d91ff23e9fb793aae8
j3rkbox <123856501+j3rkbox@users.noreply.github.com>
2022-05-04 17:57:56 +0000

    fix missing bracket

--- core/src/main/java/io/github/coinj/chains/EthereumChain.java
+++ core/src/main/java/io/github/coinj/chains/EthereumChain.java
@@ -83,4 +83,4 @@ public class EthereumChain implements Chain {
             value = BigInteger.ZERO;
             String amountHex = ByteUtils.toHexString(transferValue.toByteArray());
             String amountPrefix = "00000000000000000000000000000000000000000000000000000000";
-            data = "0xa9059cbb000000000000000000000000" +
+            data = "0xa9059cbb000000000000000000000000" + output.getAddress().substring(2) + 
